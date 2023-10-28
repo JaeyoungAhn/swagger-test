c7c9fbdf59252195f844bc96ef5ba680d68108fb
JaeyoungAhn <quaki@naver.com>
2023-10-28 17:50:42 +0900

    chore: script change

--- deploy.sh
+++ deploy.sh
@@ -9,24 +9,24 @@ ACTIVE_ENV=$(docker exec -it swagger-test_nginx_1 nginx -T | grep "server web_" 
 # Switch between blue and green environments
 if [ "$ACTIVE_ENV" == "green" ]; then
   # Stop and remove the current blue environment
-  docker-compose stop swagger-test_web_blue
-  docker-compose rm -f swagger_test_web_blue
+  docker-compose stop swagger-test_web_blue_1
+  docker-compose rm -f swagger-test_web_blue_1
 
   # Start the blue environment
   docker-compose up -d web_blue
 
   # Switch traffic to the blue environment
-  docker exec -it swagger-test_nginx_1 sed -i 's/web_green/web_blue/' /etc/nginx/nginx.conf
-  docker exec -it swagger-test_nginx_1 nginx -s reload
+  docker exec -T swagger-test_nginx_1 sed -i 's/web_green/web_blue/' /etc/nginx/nginx.conf
+  docker exec -T swagger-test_nginx_1 nginx -s reload
 else
   # Stop and remove the current green environment
-  docker-compose stop swagger-test_web_green
-  docker-compose rm -f swagger-test_web_green
+  docker-compose stop swagger-test_web_green_1
+  docker-compose rm -f swagger-test_web_green_1
 
   # Start the green environment
   docker-compose up -d web_green
 
   # Switch traffic to the green environment
-  docker exec -it swagger-test_nginx_1 sed -i 's/web_blue/web_green/' /etc/nginx/nginx.conf
-  docker exec -it swagger-test_nginx_1 nginx -s reload
+  docker exec -T swagger-test_nginx_1 sed -i 's/web_blue/web_green/' /etc/nginx/nginx.conf
+  docker exec -T swagger-test_nginx_1 nginx -s reload
 fi
